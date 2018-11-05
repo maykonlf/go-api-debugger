d564235f1c0614bdeed2aae8349f2822a7a85c72
Maykon Fonseca <maykonlfonseca@gmail.com>
2018-11-05 08:51:14 -0200

    Add appengine debug logging

--- handlers/main.go
+++ handlers/main.go
@@ -1,10 +1,28 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methos", "GET, POST, PUT, DELETE, OPTIONS")
+
+	ctx := appengine.NewContext(r)
+
+	body, _ := ioutil.ReadAll(r.Body)
+	re := regexp.MustCompile(`\s*\r?\n\s*`)
+	body = re.ReplaceAll(body, []byte{})
+
+	headers, _ := json.Marshal(r.Header)
+
+	log.Debugf(ctx, "headers: %s", headers)
+	log.Debugf(ctx, "body: %s", body)
 
 	w.WriteHeader(http.StatusOK)
 }
